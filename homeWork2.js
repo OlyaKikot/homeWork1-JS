0b3f73f02e0809ef8c399a9f8358168e4e72b2b0
Olya Kikot <kikotolya1989@gmail.com>
2022-02-03 14:22:39 +0200

    homeWork2

---
+++ homeWork2.js
@@ -0,0 +1,200 @@
+// Task 1
+// Coding in function fiveLine, function accept 1 parameter:
+// s.
+// s is a string.
+// Please return a string of 5 lines(newline symbol is \n).
+// The first line has one s;
+// Second line have two s; and so on..
+// Fifth line have five s;
+// Note1: The two sides of the parameter s may contain
+// some whitespace, please clear them before using s.
+// Note2: Using a string template can make your job easier.
+// Example:
+// fiveLine("  a") should return "a\naa\naaa\naaaa\naaaaa"
+// a
+// aa
+// aaa
+// aaaa
+// aaaaa       <---The effect when you console.log it
+// fiveLine("  xy ")
+// should return "xy\nxyxy\nxyxyxy\nxyxyxyxy\nxyxyxyxyxy"
+// xy
+// xyxy
+// xyxyxy
+// xyxyxyxy
+// xyxyxyxyxy  <---The effect when you console.log it
+function fiveLine(str) {
+  const newStr = str.trim();
+  return (
+    newStr +
+    "\n" +
+    newStr +
+    newStr +
+    "\n" +
+    newStr +
+    newStr +
+    newStr +
+    "\n" +
+    newStr +
+    newStr +
+    newStr +
+    newStr +
+    "\n" +
+    newStr +
+    newStr +
+    newStr +
+    newStr +
+    newStr +
+    "\n"
+  );
+}
+// console.log(fiveLine("a "));
+// console.log(fiveLine("  xy "));
+
+// Task 2
+// Given a string, turn each character into its ASCII character
+// code and join them together to create a number - let's call
+// this number total1:
+// 'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+// Then replace any incidence of the number 7 with the
+// number 1, and call this number 'total2':
+// total1 = 656667
+//               ^
+// total2 = 656661
+//               ^
+// Then return the difference between the sum of the digits
+// in total1 and total2:
+// (6 + 5 + 6 + 6 + 6 + 7)
+// - (6 + 5 + 6 + 6 + 6 + 1)
+// -------------------------
+//                        6
+// Example:
+// calc('ABC') => 6
+// calc('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQR
+// STUVWXYZ') => 96
+
+function calc(str) {
+  return (
+    str
+      .split("")
+      .map((val) => val.charCodeAt())
+      .join("")
+      .split("")
+      .filter((el) => el === "7").length * 6
+  );
+}
+// console.log(calc("ABC"));
+// console.log(calc("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+
+// Task 3
+// Coding in function alienLanguage, function accept 1
+// parameter:str. str is a sentence.
+// We translate the sentence into an alien language
+// according to the following rules:
+// Each word in the sentence is separated by spaces. The
+// last letter of each word in the sentence turns to lowercase,
+// and the other letters are capitalized. Looks very strange?
+// Because this is the form of alien language ;-)
+// For example:
+// alienLanguage("My name is John") should return "My
+// NAMe Is JOHn"
+// alienLanguage("this is an example") should return "THIs Is
+// An EXAMPLe"
+// alienLanguage("Hello World") should return "HELLo
+// WORLd"
+
+function alienLanguage(str) {
+  return str
+    .split(" ")
+    .map((el) => el.slice(0, -1).toUpperCase() + el.slice(-1).toLowerCase())
+    .join(" ");
+}
+// console.log(alienLanguage("My name is John"));
+// console.log(alienLanguage("Hello World"));
+// console.log(alienLanguage("this is an example"));
+// console.log("rere rere".split(""));
+
+// Task 4
+// A number is called Automorphic number if and only if its
+// square ends in the same digits as the number itself.
+// Given a number determine if it Automorphic or not .
+// Example:
+// automorphic(1),"Automorphic"
+// automorphic(3),"Not!!"
+
+function automorphic(num) {
+  let pow = Math.pow(num, 2).toString();
+  if (pow.charAt(pow.length - 1) === num.toString()) {
+    return "Automorphic";
+  }
+  return "Not!!";
+}
+// console.log(automorphic(5));
+// console.log(automorphic(1));
+// console.log(automorphic(8));
+
+// Task 5 *
+// Imagine JavaScript didn't natively include the call function.
+// The apply function however still exists.
+// Using apply, write call.
+// Note: console.log internally uses the 'call' function, which
+// therefore means you can't debug using console.log as it
+// will either call an empty function or cause an infinite loop.
+
+Function.prototype.call = function (context, ...args) {
+  Function.prototype.apply(context, args);
+};
+
+// Task 6*
+// You probably know about Function.prototype.bind method
+// in JavaScript. It returns a copy of the original function but
+// this function will always be called in the specified context.
+// The problem is that you can't rebind another context any
+// more.
+Function.prototype.bind = function (ctx) {
+  var fn = this;
+  return function () {
+    return fn.apply(this || ctx);
+  };
+};
+
+var func = function () {
+  return this.prop;
+};
+var obj1 = { prop: 1 },
+  obj2 = { prop: 2 };
+func = func.bind(obj1);
+// console.log(func()); // Will produce 1
+func = func.bind(obj2);
+// console.log(func()); // Will also produce 1 :(
+// Your task is override the
+// native Function.prototype.bind method by a new one that
+// will allow you to rebind context multiple times. In this data
+// you don't need to worry about currying, testing function
+// will never get params.
+
+// Task 7
+// Functional closures can get overly attached. Set them
+// straight!
+// Why doesn't greet_abe() actually greet Abe?
+// Code:
+// var name = "Abe";
+// var greet_abe = function () {
+//   return "Hello, " + name + "!";
+// };
+
+// name = "Ben";
+// var greet_ben = function () {
+//   return "Hello, " + name + "!";
+// };
+function greet(name) {
+  return () => {
+    return "Hello, " + name + "!";
+  };
+}
+
+const greet_abe = greet("Abe");
+const greet_ben = greet("Ben");
+
+// console.log(greet_abe());
+// console.log(greet_ben());
--- index.js
+++ index.js
@@ -1,155 +1,155 @@
-// Task 1
-// Write a function that takes an integer as input, and
-// returns the number of bits that are equal to one in the
-// binary representation of that number. You can’t guarantee
-// that input is non-negative.
-// Example: The binary representation of 1234 is
-// 10011010010, so the function should return 5 in this case.
+// // Task 1
+// // Write a function that takes an integer as input, and
+// // returns the number of bits that are equal to one in the
+// // binary representation of that number. You can’t guarantee
+// // that input is non-negative.
+// // Example: The binary representation of 1234 is
+// // 10011010010, so the function should return 5 in this case.
 
-const sumBits = function (n) {
-  const arr = n.toString(2).split("");
+// const sumBits = function (n) {
+//   const arr = n.toString(2).split("");
 
-  return arr.reduce(
-    (acc, item) => (item === "-" ? acc : acc + Number(item)),
-    0
-  );
-};
-// console.log(sumBits(-1234));
+//   return arr.reduce(
+//     (acc, item) => (item === "-" ? acc : acc + Number(item)),
+//     0
+//   );
+// };
+// // console.log(sumBits(-1234));
 
-// Task 2
-// Return an array containing the numbers from 1 to N,
-// where N is the parametered value.
-// Replace certain values however if any of the following
-// conditions are met:
-// if the value is a multiple of 3: use the value "Fizz"
-// instead
-// If the value is a multiple of 5: use the value "Buzz"
-// instead
-// If the value is a multiple of 3 & 5: use the value
-// "FizzBuzz" instead
-// N will never be less than 1.
-// Method calling example:
+// // Task 2
+// // Return an array containing the numbers from 1 to N,
+// // where N is the parametered value.
+// // Replace certain values however if any of the following
+// // conditions are met:
+// // if the value is a multiple of 3: use the value "Fizz"
+// // instead
+// // If the value is a multiple of 5: use the value "Buzz"
+// // instead
+// // If the value is a multiple of 3 & 5: use the value
+// // "FizzBuzz" instead
+// // N will never be less than 1.
+// // Method calling example:
 
-function fizzbuzz(num) {
-  let arr = [...Array(num + 1)].map((a, b) => b);
-  const newArr = arr.slice(1, arr.length);
+// function fizzbuzz(num) {
+//   let arr = [...Array(num + 1)].map((a, b) => b);
+//   const newArr = arr.slice(1, arr.length);
 
-  return newArr.map((val) => {
-    if (val % 3 === 0 && val % 5 === 0) return "FizzBuzz";
-    else if (val % 3 === 0) {
-      return "Fizz";
-    } else if (val % 5 === 0) {
-      return "Buzz";
-    } else return val;
-  });
-}
+//   return newArr.map((val) => {
+//     if (val % 3 === 0 && val % 5 === 0) return "FizzBuzz";
+//     else if (val % 3 === 0) {
+//       return "Fizz";
+//     } else if (val % 5 === 0) {
+//       return "Buzz";
+//     } else return val;
+//   });
+// }
 
-// console.log(fizzbuzz(3));
-// console.log(fizzbuzz(15));
+// // console.log(fizzbuzz(3));
+// // console.log(fizzbuzz(15));
 
-// Task 3
-// Multiply all the digits of a nonnegative integer n by each
-// other, repeating with the product until a single digit is
-// obtained. The number of steps required is known as
-// the multiplicative persistence.
-// Create a function that calculates the individual results of
-// each step, not including the original number, but including
-// the single digit, and outputs the result as a list/array. If
-// the input is a single digit, return an empty list/array.
-// Examples
-// per(1)  = []
-// per(10) = [0]
-// // 1*0 = 0
-// per(69) = [54, 20, 0]
-// // 6*9 = 54 --> 5*4 = 20 --> 2*0 = 0
-// per(277777788888899) = [4996238671872, 438939648,
-// 4478976, 338688, 27648, 2688, 768, 336, 54, 20, 0]
-// // 2*7*7*7*7*7*7*8*8*8*8*8*8*9*9 = 4996238671872 -->
-// 4*9*9*6*2*3*8*6*7*1*8*7*2 = 4478976 -->
+// // Task 3
+// // Multiply all the digits of a nonnegative integer n by each
+// // other, repeating with the product until a single digit is
+// // obtained. The number of steps required is known as
+// // the multiplicative persistence.
+// // Create a function that calculates the individual results of
+// // each step, not including the original number, but including
+// // the single digit, and outputs the result as a list/array. If
+// // the input is a single digit, return an empty list/array.
+// // Examples
+// // per(1)  = []
+// // per(10) = [0]
+// // // 1*0 = 0
+// // per(69) = [54, 20, 0]
+// // // 6*9 = 54 --> 5*4 = 20 --> 2*0 = 0
+// // per(277777788888899) = [4996238671872, 438939648,
+// // 4478976, 338688, 27648, 2688, 768, 336, 54, 20, 0]
+// // // 2*7*7*7*7*7*7*8*8*8*8*8*8*9*9 = 4996238671872 -->
+// // 4*9*9*6*2*3*8*6*7*1*8*7*2 = 4478976 -->
 
-function per(num) {
-  let res = [];
-  const arr = num.toString().split("");
-  if (arr.length === 1) {
-    return res;
-  } else {
-    const newVal = arr.reduce((a, b) => a * b, 1);
-    res.push(newVal);
-    if (newVal < 10) return res;
-    return [...res, ...per(newVal)];
-  }
-}
-// console.log(per(1));
-// console.log(per(10));
-// console.log(per(69));
-// console.log(per(277777788888899));
+// function per(num) {
+//   let res = [];
+//   const arr = num.toString().split("");
+//   if (arr.length === 1) {
+//     return res;
+//   } else {
+//     const newVal = arr.reduce((a, b) => a * b, 1);
+//     res.push(newVal);
+//     if (newVal < 10) return res;
+//     return [...res, ...per(newVal)];
+//   }
+// }
+// // console.log(per(1));
+// // console.log(per(10));
+// // console.log(per(69));
+// // console.log(per(277777788888899));
 
-// Task 4
-// Create polifills for:
-// concat()
-// lastIndexOf()
-// includes()
-// repeat()
-// substr()
-// substring()
+// // Task 4
+// // Create polifills for:
+// // concat()
+// // lastIndexOf()
+// // includes()
+// // repeat()
+// // substr()
+// // substring()
 
-function concat(array, ...args) {
-  args.forEach((val) => {
-    Array.isArray(val)
-      ? val.forEach((el) => {
-          array.push(el);
-        })
-      : array.push(val);
-  });
-  return array;
-}
+// function concat(array, ...args) {
+//   args.forEach((val) => {
+//     Array.isArray(val)
+//       ? val.forEach((el) => {
+//           array.push(el);
+//         })
+//       : array.push(val);
+//   });
+//   return array;
+// }
 
-// console.log(concat([1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]));
+// // console.log(concat([1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]));
 
-function lastIndexOf(arr, val, startFrom) {
-  for (let i = startFrom ? startFrom : arr.length; i >= 0; i--) {
-    if (arr[i] === val) return i;
-  }
-  return -1;
-}
-// console.log(lastIndexOf([1, 2, 3, 4, 5], 2, 3));
-// console.log(lastIndexOf([1, 2, 3, 4, 5], 4));
-// console.log(lastIndexOf([1, 2, 3, 4, 5], 8, 3));
+// function lastIndexOf(arr, val, startFrom) {
+//   for (let i = startFrom ? startFrom : arr.length; i >= 0; i--) {
+//     if (arr[i] === val) return i;
+//   }
+//   return -1;
+// }
+// // console.log(lastIndexOf([1, 2, 3, 4, 5], 2, 3));
+// // console.log(lastIndexOf([1, 2, 3, 4, 5], 4));
+// // console.log(lastIndexOf([1, 2, 3, 4, 5], 8, 3));
 
-function includes(array, value) {
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i] === value) {
-      return true;
-    }
-  }
-  return false;
-}
-// console.log(includes([1, 2, 3, 4, 5], 3));
-// console.log(includes([1, 2, 3, 4, 5], 8));
+// function includes(array, value) {
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (array[i] === value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// // console.log(includes([1, 2, 3, 4, 5], 3));
+// // console.log(includes([1, 2, 3, 4, 5], 8));
 
-function repeat(str, num) {
-  if (num < 0) return RangeError;
-  if (num === 0) return "";
-  else return new Array(Math.floor(num)).fill(str).join("");
-}
-// console.log(repeat("asd", 5));
-// console.log(repeat("asd", 3, 7));
+// function repeat(str, num) {
+//   if (num < 0) return RangeError;
+//   if (num === 0) return "";
+//   else return new Array(Math.floor(num)).fill(str).join("");
+// }
+// // console.log(repeat("asd", 5));
+// // console.log(repeat("asd", 3, 7));
 
-function substr(str, start, length = str.length) {
-  if (start === NaN) return str;
-  return str.slice(start, length + 1);
-}
+// function substr(str, start, length = str.length) {
+//   if (start === NaN) return str;
+//   return str.slice(start, length + 1);
+// }
 
-// console.log(substr("Mozilla", 1, 2));
-// console.log(substr("Mozilla", 2));
-// console.log(substr("Mozilla", NaN));
+// // console.log(substr("Mozilla", 1, 2));
+// // console.log(substr("Mozilla", 2));
+// // console.log(substr("Mozilla", NaN));
 
-function substring(str, indexA, indexB = str.length) {
-  if (indexA === indexB) return "";
-  if (indexB < indexA) {
-    return str.slice(indexB, indexA);
-  }
-  return str.slice(indexA, indexB);
-}
-// console.log(substring("Mozilla", 4, 7));
-// console.log(substring("Mozilla", 7, 4));
+// function substring(str, indexA, indexB = str.length) {
+//   if (indexA === indexB) return "";
+//   if (indexB < indexA) {
+//     return str.slice(indexB, indexA);
+//   }
+//   return str.slice(indexA, indexB);
+// }
+// // console.log(substring("Mozilla", 4, 7));
+// // console.log(substring("Mozilla", 7, 4));
